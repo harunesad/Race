3c9a6a6a2b3617595527ce319b2842a52de693fd
OSSY10 <ozaneser99@hotmail.com>
2022-07-28 14:42:18 +0300

    AI

---
+++ Assets/Scripts/CarAi.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAi : MonoBehaviour
+{
+    [SerializeField] Transform targetPositionTranform;
+    [SerializeField] List<Transform> waypoints = new List<Transform>();
+    [SerializeField] float xSpeed = 0;
+    [SerializeField] float zSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xSpeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void WayPoint(Collider other)
+    {
+        if (other.gameObject.CompareTag("Waypoint"))
+        {
+            waypoints.Remove(waypoints[0]);
+            targetPositionTranform = null;
+            Destroy(other.gameObject);
+            if (targetPositionTranform == null && waypoints.Count > 0)
+            {
+                targetPositionTranform = waypoints[0];
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WayPoint(other);
+    }
+    private void Movement()
+    {
+        if (waypoints.Count > 0)
+        {
+            transform.Translate(xSpeed * Time.deltaTime, 0, zSpeed * Time.deltaTime);
+            if (waypoints[0].position.x > transform.position.x)
+            {
+                xSpeed = 10f;
+            }
+            else if (waypoints[0].position.x < transform.position.x)
+            {
+                xSpeed = -10f;
+            }
+            else
+            {
+                Debug.Log("0");
+                xSpeed = 0;
+            }
+        }
+        
+    }
+}
